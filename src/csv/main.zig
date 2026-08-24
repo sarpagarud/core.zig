dd87d00cb7380f3d79c0f441aa895ff7d74a269f
sarpagarud <241446179+sarpagarud@users.noreply.github.com>
2026-08-24 14:00:28 +0530

    Update main.zig

--- src/csv/main.zig
+++ src/csv/main.zig
@@ -1,12 +1,13 @@
 const std = @import("std");
 
-const Csv = struct {
+pub const Csv = struct {
   file: []const u8,
   arena: std.heap.ArenaAllocator,
   headers: [][]const u8 = .{},
   rows: std.ArrayList(std.StringHashMap([]const u8)) = .{},
 
-  fn get_csv_line(
+  fn get_csv_line_str(
+    self: *Csv,
     list: *std.ArrayList([]const u8),
     seperator: []const u8
   ) ![]const u8 {
@@ -14,36 +15,40 @@ const Csv = struct {
     return try std.mem.join(allocator, seperator, list.items);
   }
 
-  fn get_csv_lines(
+  fn get_csv_lines_str(
+    self: *Csv,
     list: *std.ArrayList([]const u8),
     seperator: []const u8
   ) ![]const u8 {
     const allocator = self.arena_allocator();
     var csv_list: std.ArrayList([]const u8) = .empty;
-    for(row.items) |h|{
-      var it = self.rows.iterator();
+    for(list.items) |h|{
+      var it = h.iterator();
       var l: std.ArrayList([]const u8) = .empty;
       while (it.next()) |e| {
         l.appendSlice(allocator, e.value_ptr.*);
       }
-      var s = try self.get_csv_line(l, seperator);
+      const s = try self.get_csv_line_str(l, seperator);
       csv_list.appendSlice(allocator, s);
       l.clearRetainingCapacity();
     }
     csv_list.clearRetainingCapacity();
-    return self.get_csv_line(csv_list, "\n");
+    return self.get_csv_line_str(csv_list, "\n");
   }
 
   fn save_csv(
+    self: *Csv,
     file: []const u8,
     data: []const u8
   ) !void {
+    _ = self;
     const f = try std.fs.cwd().openFile(file, .{ .mode = .write_only });
     defer f.close();
     try f.writeAll(data);
   }
 
   fn get_csv_line(
+    self: *Csv,
     list: *std.ArrayList([]const u8), 
     line: []const u8,
     seperator: []const u8
@@ -52,13 +57,13 @@ const Csv = struct {
     const allocator = self.arena_allocator();
     var cols = std.mem.splitScalar(u8, line, seperator);
     while (cols.next()) |col| {
-      try list.append(allocator, try a.dupe(
+      try list.append(allocator, try allocator.dupe(
         u8, std.mem.trim(u8, col, " \r") 
       ));
     }
   }
 
-  fn read_csv(self: *Csv) void {
+  fn read_csv(self: *Csv, io: std.Io,) void {
     const allocator = self.arena_allocator();
     const text = try std.Io.Dir.cwd().readFileAlloc(
       io,
@@ -77,9 +82,9 @@ const Csv = struct {
       var row_map = std.StringHashMap([]const u8).init(allocator);
       try get_csv_line(allocator, &row, line);
       for (row.items, 0..) |value, j| {
-        try row_map.put(headers.items[j], value);
+        try row_map.put(self.headers.items[j], value);
       }
-      try rows.append(allocator, row_map);
+      try self.rows.append(allocator, row_map);
     }
   }
 
@@ -90,7 +95,7 @@ const Csv = struct {
   pub fn init(self: *Csv, allocator: std.mem.Allocator, file: []const u8) Csv {
     return .{
       .arena = std.heap.ArenaAllocator.init(allocator),
-      .file: try file.toOwnedSlice(self.allocator);
+      .file = try file.toOwnedSlice(self.allocator),
     };
   }
 
@@ -98,7 +103,7 @@ const Csv = struct {
     self.arena.deinit();
   }
 
-}
+};
 
 pub fn hello() !void {
   std.debug.print("{s}\n", .{"hello from csv!"});
